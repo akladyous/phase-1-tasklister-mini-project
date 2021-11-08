4b26ec4f5d173f601be8240bc781261dd84c82e2
boula akladyous <akladyous@gmail.com>
2021-11-08 12:08:56 -0500

    completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
+
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  let form = document.querySelector('#create-task-form');
+  form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    handleToDo(event.target.new_task_description.value);
+    form.reset();
+  });
+  
 });
+
+function handleToDo(toDoValue){
+  let tasks = document.querySelector('#tasks');
+  let newTask = document.createElement('li')
+  newTask.textContent = `${toDoValue} `;
+  let bttn = document.createElement('button')
+  bttn.addEventListener('click', deleteTask)
+  bttn.textContent = 'x';
+  newTask.append(bttn);
+  tasks.appendChild(newTask, bttn);
+};
+
+function deleteTask(event){
+  console.log(event.parentNode);
+  event.target.parentNode.remove();
+};
